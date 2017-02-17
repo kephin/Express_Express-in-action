702b66757fa181389ada0bfa569e600e0670085f
kephin <wunshon76325@gmail.com>
2017-02-17 23:46:10 +0800

    parsing the request URL for individual route

--- basic/basic_server.js
+++ basic/basic_server.js
@@ -4,7 +4,13 @@ const port = 3000;
 
 const requestHandler = (req, res) => {
   console.log(`In comes a request to: ${req.url}`);
-  res.end('hello world');
+  if (req.url === '/') {
+    res.end(('Welcome to the homepage.'));
+  } else if (req.url === '/about') {
+    res.end('Welcome to the about page.');
+  } else {
+    res.end('Error! File not found.');
+  }
 };
 
 const server = http.createServer(requestHandler);
