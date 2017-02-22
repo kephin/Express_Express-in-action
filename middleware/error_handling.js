051fbb729ddc49dcc0a513fdc7d69dc07afae050
kephin <wunshon76325@gmail.com>
2017-02-22 23:12:24 +0800

    error-handling middleware

---
+++ middleware/error_handling.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const filePath = path.join(__dirname, 'test.jpg');
+
+app.use((req, res, next) => {
+  res.sendFile(filePath, err => {
+    //enter error mode by calling next() with an arguement
+    if (err) return next(new Error(err));
+    console.log('File sent!');
+  });
+});
+
+//error-handling middleware
+app.use((err, req, res, next) => {
+  console.log(err);
+  next(err);
+});
+
+//here, we must pass 4 arguments so Express know this is a error-handling middleware
+app.use((err, req, res, next) => {
+  res.status(500).send('Internal server error.');
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000...');
+});
