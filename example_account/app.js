b908484b52b3686820803aeb0843546771df49d6
kephin <wunshon76325@gmail.com>
2017-03-16 00:03:13 +0800

    set up app.js

---
+++ example_account/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const flash = require('connect-flash');
+
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/AccoutApp');
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({
+  secret: 'hello_world',
+  resave: true,
+  saveUninitialized: true,
+}));
+app.use(flash());
+app.use(routes);
+
+app.listen(app.get('port'), () => {
+  console.log(`Server started on port ${app.get('port')}`);
+});
