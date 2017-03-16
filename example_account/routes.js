c64c8073fef37f69157cff34c70dfc8139de3987
kephin <wunshon76325@gmail.com>
2017-03-16 23:36:00 +0800

    fix typo and bugs

--- example_account/routes.js
+++ example_account/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const User = require('./models/user');
 const passport = require('passport');
+
+const User = require('./models/user');
 
 const router = express.Router();
 
@@ -13,10 +14,11 @@ router.use((req, res, next) => {
 
 router.get('/', (req, res, next) => {
   User.find()
-    .sort({ createAt: 'descending' })
+    .sort({ createdAt: 'descending' })
     .exec((err, users) => {
       if (err) return next(err);
-      res.render('index', { users: users });
+      res.render('index', { users });
+      // res.json(users);
     });
 });
 
@@ -28,22 +30,19 @@ router.post('/signup', (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  User.findOne({ username: username }, (err, user) => {
+  User.findOne({ username }, (err, user) => {
     if (err) return next(err);
     if (user) {
       req.flash('error', 'User already exists!');
       return res.redirect('/signup');
     }
-    const newUser = new User({
-      username: username,
-      password: password,
-    });
+    const newUser = new User({ username, password });
     newUser.save(next);
   });
 }, passport.authenticate('login', {
   successRedirect: '/',
   failureRedirect: '/signup',
-  failureFlash: 'true',
+  failureFlash: true,
 }));
 
 module.exports = router;
