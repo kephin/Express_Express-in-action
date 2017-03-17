fbd69b920daec5c9d8574c433bcc191351293369
kephin <wunshon76325@gmail.com>
2017-03-17 23:31:47 +0800

    add profile route and view

--- example_account/routes.js
+++ example_account/routes.js
@@ -45,4 +45,12 @@ router.post('/signup', (req, res, next) => {
   failureFlash: true,
 }));
 
+router.get('/users/:username', (req, res, next) => {
+  User.findOne({ username: req.params.username }, (err, user) => {
+    if (err) return next(err);
+    if (!user) return next(404);
+    res.render('profile', { user });
+  });
+});
+
 module.exports = router;
