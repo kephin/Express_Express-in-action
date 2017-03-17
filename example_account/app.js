d5c50713d3b043f54a73f0459306e5b4fca87cb9
kephin <wunshon76325@gmail.com>
2017-03-17 23:43:06 +0800

    set up passport middlewares

--- example_account/app.js
+++ example_account/app.js
@@ -3,14 +3,16 @@ const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const passport = require('passport');
 const session = require('express-session');
 const flash = require('connect-flash');
 
 const routes = require('./routes');
+const setUpPassport = require('./setuppassport');
 
 const app = express();
-
 mongoose.connect('mongodb://localhost:27017/AccoutApp');
+setUpPassport();
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +26,8 @@ app.use(session({
   saveUninitialized: true,
 }));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(routes);
 
 app.listen(app.get('port'), () => {
---
+++ example_account/setuppassport.js
@@ -0,0 +1,29 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const User = require('./models/user');
+
+module.exports = () => {
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+      done(err, user);
+    });
+  });
+
+  passport.use('login', new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username.trim() }, (err, user) => {
+      if (err) return done(err);
+      if (!user) return done(null, false, { message: 'No user has that username!' });
+
+      user.checkPassword(password, (err, isMatch) => {
+        if (err) return done(err);
+        if (!isMatch) return done(null, false, { message: 'Invalid password.' });
+        return done(null, user, { message: 'Successfully logged in.' });
+      });
+    });
+  }));
+};
